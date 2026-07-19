eeeacadf9d5cf0165b9f82e1ccebf5b2efcf67e7
Harsh Kumar Jha <harhkrjha.2908@gmail.com>
2026-07-19 22:41:23 +0530

    small

--- learn with react/src/App.jsx
+++ learn with react/src/App.jsx
@@ -2,9 +2,55 @@ import React from 'react'
 import Card from "./component/Card";
 
  const App = () => {
+  const jobs = [
+  {
+    id: 1,
+    company: "Google",
+    logo: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png",
+    role: "Frontend Developer",
+    field: "Web Development",
+    skills: [ "JavaScript"],
+    rating: 4.8,
+    saved: true,
+  },
+  {
+    id: 2,
+    company: "Microsoft",
+    logo: "https://cdn-icons-png.flaticon.com/512/732/732221.png",
+    role: "Backend Developer",
+    field: "Node.js",
+    skills: ["Node"],
+    rating: 4.7,
+    saved: false,
+  },
+  {
+    id: 3,
+    company: "Amazon",
+    logo: "https://cdn-icons-png.flaticon.com/512/5968/5968870.png",
+    role: "Full Stack Developer",
+    field: "MERN Stack",
+    skills: ["React"],
+    rating: 4.9,
+    saved: true,
+  },
+  {
+    id: 4,
+    company: "Netflix",
+    logo: "https://cdn-icons-png.flaticon.com/512/5977/5977590.png",
+    role: "UI/UX Designer",
+    field: "Design",
+    skills: ["Figma"],
+    rating: 4.6,
+    saved: false,
+  },
+];
+
+
+
   return (
-   <div className="parent"><Card />
-   <Card /> <Card /></div>
-  )
-}
+  <div className="parent">
+      {jobs.map((elem) => {
+        return <Card key={elem.id} skill={elem.skills} />;
+      })}
+    </div>)}
 export default App;
--- learn with react/src/component/Card.jsx
+++ learn with react/src/component/Card.jsx
@@ -1,23 +1,40 @@
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500"
-        alt="Profile"
-      />
 
-      <div className="card-content">
-        <h2>harsh</h2>
+      <div className="top">
 
-        <p>
-          Frontend Developer passionate about React and modern web
-          development.
-        </p>
+        <div className="company">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png"
+            alt="logo"
+          />
+        </div>
 
-        <button>View Profile</button>
+        <button className="save">🔖</button>
+
       </div>
+
+      <div className="middle">
+        <h2>Frontend Developer</h2>
+        <p>Google</p>
+
+        <div className="details">
+          <span>{props.skill}</span>
+          <span>Remote</span>
+          <span>2+ Years</span>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="bottom">
+        <h3>⭐ 4.8</h3>
+        <button className="apply">Apply Now</button>
+      </div>
+
     </div>
   );
 }
